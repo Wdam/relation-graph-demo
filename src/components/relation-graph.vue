83da28a050bfcf8b4834cfaf7aa94c118dca9fb6
关利涛 <mr.g0space@gmail.com>
2024-03-18 15:16:51 +0800

    1

--- src/components/relation-graph.vue
+++ src/components/relation-graph.vue
@@ -1,16 +1,57 @@
 <template>
   <div>
-    <div style="height: 100vh" :class="['my-line-style-2']">
+    <div style="height: 100vh">
       <RelationGraph ref="graphRef" :options="graphOptions" :on-node-click="onNodeClick" :on-line-click="onLineClick">
         <template #node="{node}">
           <div >
-            <div class="node-body" @mouseenter="handelMouseenter" @mouseleave="handelMouseleave">
+            <div class="node-body" @mouseenter="handelMouseenter(node,$event)" @mouseleave="handelMouseleave(node,$event)">
                 <div class="node-body-item" :style="[{backgroundColor:(node.data.direction==='IN'?'rgb(255, 96, 96)':'rgb(18, 139, 237)')},{color:'#ffffff'}]">{{node.text}}</div>
-               <div class="node-body-item" v-for="i in node.data.collection" :key="i.id">
-                 {{i.nodeName}}
+               <div class="node-body-item" v-for="(i,index) in node.data.collection" :key="i.id" v-if="index < 3">
+                 <span>{{i.nodeName}}</span>
                </div>
+              <span @click="detailOpen = true" v-if="node.data.collection" class="node-body-item">查看全部{{node.data.collection.length}}家人员/企业</span>
             </div>
-<!--            <span class="operation" :style="[{color:(node.data.direction==='IN'?'rgb(255, 96, 96)':'rgb(18, 139, 237)')}]">{{node.data.operation}}</span>-->
+          </div>
+        </template>
+        <template slot="graph-plug" >
+          <div class="item-detail" v-if="detailOpen">
+            <div class="detail-header">
+              <span>{{listTitle}}</span>
+              <span class="close" @click="detailOpen = false">×</span>
+            </div>
+            <div class="item-search">
+              <input  v-model="searchValue" />
+              <span style="cursor: pointer" @click="openAll">{{openText}}</span>
+            </div>
+            <div class="item-list">
+              <div v-for="(i,index) in listData1" :key="i.id">
+                <div class="item-list-top" @click="openDetail(i)">
+                  <div class="item-list-avater">
+                    {{i.nodeName[0]}}
+                  </div>
+                  <div class="item-list-title">
+                    <span>
+                     {{index + 1}}.{{i.nodeName}}
+                    </span>
+                  </div>
+                  <div class="item-list-open">
+                    <img src="../assets/open.png">
+                  </div>
+                </div>
+                <div class="item-list-detail" v-if="i.open">
+                  <div v-for="j in i.paths" :key="j.id">
+                    <div class="company-1">
+                      <span>{{j.nodeName}}</span>
+                    </div>
+                    <div class="link" v-if="j.operation">
+                      <span>{{j.operation}}</span>
+                      <span class="link-line"></span>
+                      <span>{{j.reason}}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </template>
       </RelationGraph>
@@ -25,6 +66,10 @@ export default {
   components: {RelationGraph },
   data() {
     return {
+      openText:'全部展开',
+      detailOpen:false,
+      listTitle:'',
+      listData:[],
       chrildNode:[],
       lines:[],
       rootNode:{id: data['id'].toString(), text: data['nodeName'],index:0 },
@@ -53,10 +98,16 @@ export default {
         // defaultNodeHeight: 50,
         defaultJunctionPoint: 'tb',
       },
+      searchValue:''
     }
   },
   mounted() {
     this.showGraph()
+  },
+  computed:{
+    listData1(){
+      return this.searchValue === ''? this.listData:this.listData.filter(a => a.nodeName.indexOf(this.searchValue) >= 0)
+    }
   },
   methods: {
    async showGraph() {
@@ -78,8 +129,6 @@ export default {
      jsonData.nodes = this.chrildNode
      jsonData.lines =this.lines
 
-
-      console.log(jsonData.nodes,'2222')
       // 以上数据中的node和link可以参考"Node节点"和"Link关系"中的参数进行配置
       // this.$refs.graphRef.setJsonData(jsonData, (graphInstance) => {
       //   // Called when the relation-graph is completed
@@ -103,7 +152,7 @@ export default {
                   text: item.title,
                   index: depth,
                   data:{
-                    collection: item.collection.length > 4 ? item.collection.slice(0, 3) : item.collection,
+                    collection: item.collection,
                     operation:item.operation,
                     direction:item.direction,
                   },
@@ -176,14 +225,24 @@ export default {
   },
 
     onNodeClick(nodeObject, $event) {
-      console.log('onNodeClick:', nodeObject)
-      console.log('onNodeClick:', nodeObject);
+     if(nodeObject.data.collection){
+       this.listData = nodeObject.data.collection
+       this.listTitle = nodeObject.text
+       console.log('onNodeClick:', this.listData)
+
+       this.listData.forEach(item =>{
+         if(item.paths){
+           item.open = false
+         }
+       })
+     }
       const allLinks = this.$refs.graphRef.getLinks();
       allLinks.forEach(link => { // 还原所有样式
         link.relations.forEach(line => {
           line.color = "#c0c0c0";
-          line.lineWidth = 3;
-          line.animation = 0
+          line.lineWidth = 2;
+          line.className = ''
+          line.disableDefaultClickEffect = true
         });
       });
 
@@ -202,18 +261,18 @@ export default {
         allLinks.filter(link => (link.fromNode === item.fromNode && link.toNode === item.toNode)).forEach(link => {
           console.log(link,'22222')
           link.relations.forEach(line => {
-            line.color = "#ffa00b"
-            line.lineWidth = 5;
-            line.animation = 2
+            line.color = "#148ccd"
+            line.lineWidth = 3;
+            line.className = 'all_height_light'
           });
         });
       })
       fromNodeList.forEach(item=>{
         allLinks.filter(link => (link.fromNode === item.fromNode && link.toNode === item.toNode)).forEach(link => {
           link.relations.forEach(line => {
-            line.color = "#ffa00b"
-            line.lineWidth = 5;
-            line.animation = 2
+            line.color = "#148ccd"
+            line.lineWidth = 3;
+            line.className = 'all_height_light'
           });
         });
       })
@@ -235,9 +294,22 @@ export default {
     },
     onLineClick(lineObject, $event) {
       console.log('onLineClick:', lineObject)
+      lineObject['className'] = ''
     },
     handelMouseenter(nodeObject,$event){
-
+      this.onNodeClick(nodeObject,$event)
+      //console.log(nodeObject,$event,'111111')
+    },
+    handelMouseleave(){
+      const allLinks = this.$refs.graphRef.getLinks();
+      allLinks.forEach(link => { // 还原所有样式
+        link.relations.forEach(line => {
+          line.color = "#c0c0c0";
+          line.lineWidth = 2;
+          line.className = ''
+          line.disableDefaultClickEffect = true
+        });
+      });
     },
     getFromNode(allLinks, node, fromNodeList) {
       let linkList = allLinks.filter(link => (link.toNode === node));
@@ -252,9 +324,25 @@ export default {
         toNodeList.push({fromNode: node, toNode: link.toNode});
       });
     },
-    handelMouseleave(){
-
-    }
+    openDetail(item){
+      this.listData.forEach(i =>{
+        if(i.id === item.id){
+          i.open = !i.open
+        }
+      })
+      this.$forceUpdate()
+    },
+    openAll(){
+      this.listData.forEach(i =>{
+          i.open = !i.open
+        if(i.open){
+          this.openText = '全部收起'
+        }else {
+          this.openText = '全部展开'
+        }
+      })
+      this.$forceUpdate()
+    },
   }
 }
 </script>
@@ -295,18 +383,167 @@ export default {
       }
     }
 }
-//.operation::after {
-//  content: '';
-//  display: block;
-//  width: 0;
-//  height: 0;
-//  border-left: 10px solid transparent; /* 左边透明 */
-//  border-right: 10px solid transparent; /* 右边透明 */
-//  border-bottom: 10px solid black; /* 底部为实心 */
-//}
+.item-detail{
+  height: 600px;
+  width: 500px;
+  background-color: #42b983;
+  position: absolute;
+  z-index: 100;
+  top: 90px;
+  right: 20px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 4px 0px;
+  .detail-header{
+    height: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 15px;
+    box-sizing: border-box;
+    border-bottom: 1px solid #E5E5E5;
+    span{
+      font-size: 14px;
+      color: #128bed;
+      font-weight: 700;
+    }
+    .close{
+      color: #128BED;
+      font-size: 30px;
+      line-height: 52px;
+      float: right;
+      cursor: pointer;
+    }
+
+  }
+  .item-search{
+    height: 66px;
+    background-color: #ffffff;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 15px;
+    color: #666666;
+    font-size: 14px;
+    border-bottom: 1px solid #E5E5E5;
+  }
+  .item-list{
+    height: 484px;
+    width: 100%;
+    background-color: #eeeeee;
+    box-sizing: border-box;
+    overflow-x: auto;
+    .item-list-top{
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      background-color: #ffffff;
+      padding: 14px 15px;
+      .item-list-avater{
+        width: 30px;
+        height: 30px;
+        display: block;
+        border-radius: 3px;
+        float: left;
+        border: 1px solid #eee;
+        -o-object-fit: contain;
+        object-fit: contain;
+        text-align: center;
+        line-height: 30px;
+        background: #51a3d8;
+        color: #fff;
+      }
+      .item-list-title{
+        margin-left: 15px;
+        font-size: 14px;
+      }
+      .item-list-open{
+        margin-left: auto;
+      }
+    }
+    .item-list-detail{
+
+      box-sizing: border-box;
+      //height: 90px;
+      width: 100%;
+      background-color: #F3F9FD;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 14px 15px;
+      .company-1{
+        max-width: 90px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+      }
+      .link{
+        box-sizing: border-box;
+        height: 100%;
+        width: 90px;
+        //background-color: #42b983;
+        padding: 0 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        line-height: 12px;
+        color: #128bed;
+        text-align: center;
+        font-size: 12px;
+        margin-bottom: 7px;
+
+        .link-line{
+          width: 100%;
+          height: 2px;
+          background-color: #ddd;
+          margin: 5px 0;
+        }
+      }
+    }
+
+  }
+}
+</style>
+<style scoped lang="less">
+
+::v-deep .my-line-style-1{
+  .c-rg-line-checked {
+    animation: my-line-anm1 2s linear infinite;
+  }
+}
 ::v-deep .my-line-style-2{
   .c-rg-line-checked {
     animation: my-line-anm2 1s infinite;
+  }
+}
+::v-deep .my-line-style-3{
+  .c-rg-line-checked {
+    animation: my-line-anm3 1s infinite;
+  }
+}
+::v-deep .my-line-style-4{
+  .c-rg-line-checked {
+    animation: my-line-anm4 1s infinite;
+  }
+}
+::v-deep .relation-graph {
+  .all_height_light{
+    animation: my-line-anm1 1s linear infinite !important;
+  }
+}
+
+@keyframes my-line-anm1 {
+  0% {
+    stroke-dashoffset: 100px;
+    stroke-dasharray: 5, 5, 5;
+  }
+  100% {
+    stroke-dasharray: 5, 5, 5;
+    stroke-dashoffset: 3px;
   }
 }
 @keyframes my-line-anm2 {
@@ -318,5 +555,21 @@ export default {
     stroke-dashoffset: 10px;
     stroke-dasharray: 20,20,20;
   }
+}
+@keyframes my-line-anm3 {
+  0% { stroke-opacity: 1; }
+  50% { stroke-opacity: 0.2; }
+  100% { stroke-opacity: 1; }
+}
+@keyframes my-line-anm4 {
+  0%{
+    stroke-dasharray:0,100%;
+  }
+  100%{
+    stroke-dasharray:100%,0;
+  }
+}
+::v-deep .rel-link-peel>g>g{
+  transform:  translate(-1000, -1127) rotate(0deg)!important;
 }
 </style>
